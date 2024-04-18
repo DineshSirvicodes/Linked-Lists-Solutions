ef92a2fbc5e20e73bff159f7cf8f8190d6bdd12d
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-04-18 22:14:25 +0530

    Create MiddleoftheLL.cpp

---
+++ MiddleoftheLL.cpp
@@ -0,0 +1,37 @@
+// Brute force - TC - O(N+N/2) SC - O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int n = 0; // calculating the size of the LL
+        ListNode* temp = head;
+        while(temp!=NULL){
+            n++;
+            temp=temp->next;
+        }
+        int middle = (n/2)+1; // middle node size
+        temp = head;
+        while(temp!=NULL){
+            middle = middle-1;
+            if(middle == 0){
+                break;
+            }
+            temp = temp->next;
+        }
+        return temp;
+    }
+};
+//Optimal Sol - Tortoise & Hare Algorithm TC - O(n/2) Sc = O(1)
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!= NULL && fast->next!=NULL){
+          // for even sized LL - the fast will point to Null eventually then stop
+            // for odd sized LL - the fast will point to last node , therfore the next will be null, so stop
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
