792f684ec4c211ebd5dc8a7ee6e2fe54a0a19fe9
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2024-05-03 22:25:51 +0530

    Update PalindromeLinkedList.cpp

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -21,4 +21,49 @@ bool isPalindrome(ListNode* head) {
         return true;
     }
 
-// Optimal sol - 
+// Optimal sol - TC- O(2*N) - twice linkelist traversal - SC - O(1)
+// First using Tortoise and hare algo, find the node(slow) from where we compare the elements on reversal
+// Second reverse the node from second->next till last, and compare the first and the newHead and move ahead
+// for even cases - fast should be moving till second last only cuz there are 2 midpoints, so only the first is considered
+// for odd cases the fast->next ie fast should be till last position, next will be null
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* head){
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp!=NULL){
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;         
+        }
+        return prev;
+
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* first = head;
+        // for even fast should stop at second last
+        // for odd the fast should stop at last
+        while(fast->next !=NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = ReverseList(slow->next);
+        ListNode* second = newHead;
+        while(second!=NULL){
+            if(first->val != second->val){
+                ReverseList(newHead);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
+        }
+        ReverseList(newHead);
+        return true;
+    }
+};
